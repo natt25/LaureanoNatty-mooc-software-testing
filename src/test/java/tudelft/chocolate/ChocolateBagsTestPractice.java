f8e8ab4d6141381184f7c3d4988e1f4675476360
natt25 <nattyjimena@gmail.com>
2025-03-22 10:31:38 -0500

    realizando pruebas de ChoccolateBags

--- src/test/java/tudelft/chocolate/ChocolateBagsTestPractice.java
+++ src/test/java/tudelft/chocolate/ChocolateBagsTestPractice.java
@@ -5,7 +5,14 @@ public class ChocolateBagsTestPractice {
         ChocolateBags cb = new ChocolateBags();
 
         int result1 = cb.calculate(5, 3, 17);
+        System.out.println("Resultado: " + result1); // sale 2
 
-        System.out.println("Resultado: " + result1);
+        int result2 = cb.calculate(1, 1, 10);
+        System.out.println("Resultado: " + result2); // sale -1
+
+        int result3 = cb.calculate(1, 1, 7);
+        System.out.println("Resultado: " + result3); // sale -1
+
+
     }
 }
