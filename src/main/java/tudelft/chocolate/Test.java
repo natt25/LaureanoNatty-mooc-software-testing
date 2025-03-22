c39812f265f89ef9f49da4aa59b74c3c68d81521
natt25 <nattyjimena@gmail.com>
2025-03-22 09:51:28 -0500

    Test1 de clase ChocolateBags

---
+++ src/main/java/tudelft/chocolate/Test.java
@@ -0,0 +1,10 @@
+package tudelft.chocolate;
+public class Test {
+    public static void main(String[] args) {
+        ChocolateBags cb = new ChocolateBags();
+
+        int result1 = cb.calculate(5, 3, 17);
+
+        System.out.println("Resultado: " + result1);
+    }
+}
