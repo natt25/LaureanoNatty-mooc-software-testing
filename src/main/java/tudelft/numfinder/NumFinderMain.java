b7025588c208f152e1b08da596099fe21ca204e9
natty <nattyjimena@gmail.com>
2025-03-24 21:41:15 -0500

    Lab1: Ejercicios 4, Arreglar el error en la numFinder

--- src/main/java/tudelft/numfinder/NumFinderMain.java
+++ src/main/java/tudelft/numfinder/NumFinderMain.java
@@ -2,6 +2,12 @@ package tudelft.numfinder;
 
 public class NumFinderMain {
 
+    // El codigo convierte numeros romanos a enteros y encuentra
+    // el numero mas pequeño y mas grande en un arreglo de enteros.
+    // La clase NumFinder usa el método find para actualizar smallest y largest.
+    // En NumFinderMain, una llamada a find falla debido a un error
+    // en la logica del método find, donde 'else if' debería ser 'if'.
+
     public static void main (String[] args) {
         NumFinder nf = new NumFinder();
 
@@ -15,3 +21,4 @@ public class NumFinderMain {
         System.out.println(nf.getSmallest());
     }
 }
+
