d0eeb2eb3b3e12671efdf8faee24f55942964034
natt25 <nattyjimena@gmail.com>
2025-03-28 17:33:55 -0500

    EJERCICIO 3: Cifrado Caesar corregido en clase CaesarCipherShift y pruebas realizadas en CaesarCipherShiftTest

--- src/main/java/tudelft/caesarshift/CaesarShiftCipher.java
+++ src/main/java/tudelft/caesarshift/CaesarShiftCipher.java
@@ -11,16 +11,24 @@ public class CaesarShiftCipher {
 
         for(int i = 0; i < length; i++){
             currentChar = message.charAt(i);
-           
-            sb.append(currentChar);
-            if (currentChar > 'z' || currentChar < 'a') {
-                return "invalid";
-            } else if ((char) (currentChar + shift) > 'z') {
-                currentChar = (char) (currentChar - 26);
-            } else if ((char) (currentChar + shift) < 'a'){
-                currentChar = (char) (currentChar + 26);
+
+            if (currentChar == ' ') {
+                sb.append(currentChar); // Mantener espacios sin cambios
+            } else if (currentChar >= 'a' && currentChar <= 'z') {
+                char shiftedChar = (char) (((currentChar - 'a' + shift + 26) % 26) + 'a');
+                sb.append(shiftedChar);
+            } else {
+                return "invalid"; // Devolver "invalid" si hay un carácter no permitido
             }
-            sb.append((char) (currentChar + shift));
+//            sb.append(currentChar);
+//            if (currentChar > 'z' || currentChar < 'a') {
+//                return "invalid";
+//            } else if ((char) (currentChar + shift) > 'z') {
+//                currentChar = (char) (currentChar - 26);
+//            } else if ((char) (currentChar + shift) < 'a'){
+//                currentChar = (char) (currentChar + 26);
+//            }
+//            sb.append((char) (currentChar + shift));
         }
 
         return sb.toString();
--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,62 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CaesarShiftCipherTest {
 
-}
+        @Test
+        public void testSimpleShift() {
+            CaesarShiftCipher cipher = new CaesarShiftCipher();
+            assertEquals("def", cipher.CaesarShiftCipher("abc", 3));
+        }
+
+        @Test
+        public void testWrapAround() {
+            CaesarShiftCipher cipher = new CaesarShiftCipher();
+            assertEquals("abc", cipher.CaesarShiftCipher("xyz", 3));
+        }
+
+        @Test
+        public void testNegativeShift() {
+            CaesarShiftCipher cipher = new CaesarShiftCipher();
+            assertEquals("xyz", cipher.CaesarShiftCipher("abc", -3));
+        }
+
+        @Test
+        public void testZeroShift() {
+            CaesarShiftCipher cipher = new CaesarShiftCipher();
+            assertEquals("hello world", cipher.CaesarShiftCipher("hello world", 0));
+        }
+
+        @Test
+        public void testWithSpaces() {
+            CaesarShiftCipher cipher = new CaesarShiftCipher();
+            assertEquals("ifmmp xpsme", cipher.CaesarShiftCipher("hello world", 1));
+        }
+
+        @Test
+        public void testInvalidCharacters() {
+            CaesarShiftCipher cipher = new CaesarShiftCipher();
+            assertEquals("invalid", cipher.CaesarShiftCipher("hello!", 3));
+        }
+
+        @Test
+        public void testLargePositiveShift() {
+            CaesarShiftCipher cipher = new CaesarShiftCipher();
+            assertEquals("abc", cipher.CaesarShiftCipher("xyz", 29)); // 29 ≡ 3 mod 26
+        }
+
+        @Test
+        public void testLargeNegativeShift() {
+            CaesarShiftCipher cipher = new CaesarShiftCipher();
+            assertEquals("xyz", cipher.CaesarShiftCipher("abc", -29)); // -29 ≡ -3 mod 26
+        }
+
+        @Test
+        public void testOnlySpaces() {
+            CaesarShiftCipher cipher = new CaesarShiftCipher();
+            assertEquals("   ", cipher.CaesarShiftCipher("   ", 5));
+        }
+    }
+
