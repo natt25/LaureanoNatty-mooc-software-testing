631eac8d670ff3d424aa61a92d72ce3001c1293e
natty <nattyjimena@gmail.com>
2025-05-01 23:47:44 -0500

    fix: correccion de lógica de comparación en mirrorEnds

--- src/main/java/tudelft/mirror/Mirror.java
+++ src/main/java/tudelft/mirror/Mirror.java
@@ -7,15 +7,18 @@ public class Mirror {
 
         int begin = 0;
         int end = string.length() - 1;
-        for (; begin < end; begin++, end--) {
+        //for (; begin < end; begin++, end--) {
+        for (; begin < string.length(); begin++, end--) {
             if (string.charAt(begin) == string.charAt(end)) {
-                mirror += String.valueOf(string.charAt(end));
+                //mirror += String.valueOf(string.charAt(end));
+                mirror += string.charAt(begin);
             }
             else {
                 break;
             }
         }
 
-        return begin == end ? string : mirror;
+        //return begin == end ? string : mirror;
+        return mirror;
     }
 }
--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,49 @@
 package tudelft.mirror;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MirrorTest {
+
+    @Test
+    public void testBasicMirror() {
+        Mirror mirror = new Mirror();
+        assertEquals("ab", mirror.mirrorEnds("abXYZba"));
+    }
+
+    @Test
+    public void testSingleCharMirror() {
+        Mirror mirror = new Mirror();
+        assertEquals("a", mirror.mirrorEnds("abca"));
+    }
+
+    @Test
+    public void testFullMirrorOddLength() {
+        Mirror mirror = new Mirror();
+        assertEquals("aba", mirror.mirrorEnds("aba"));
+    }
+
+    @Test
+    public void testFullMirrorEvenLength() {
+        Mirror mirror = new Mirror();
+        assertEquals("abba", mirror.mirrorEnds("abba"));
+    }
+
+    @Test
+    public void testNoMirror() {
+        Mirror mirror = new Mirror();
+        assertEquals("", mirror.mirrorEnds("xyz"));
+    }
+
+    @Test
+    public void testEmptyString() {
+        Mirror mirror = new Mirror();
+        assertEquals("", mirror.mirrorEnds(""));
+    }
+
+    @Test
+    public void testSingleCharacter() {
+        Mirror mirror = new Mirror();
+        assertEquals("x", mirror.mirrorEnds("x"));
+    }
 }
